374de03dbc7a5ab44024d366b0ee63db81b3cb2d
philosofool <lenhart.lenhart%40gmail.com>
2020-07-27 15:47:15 -0700

    Created Stat and testing
    Created stats with StatCalculator class

---
+++ CBS_fantasy_baseball/stats.py
@@ -0,0 +1,280 @@
+import numpy as np
+
+class StatCalculator:
+    '''Provides basic functionality for getting rate and counting stats from existing stats.
+    Includes pre-defined calculator for common and fantasy-relevant stats.
+
+    Class Attributes
+    ----------
+    Note: it is strongly recommned that users modify the class attributes rather
+    instance
+    lgERA: numeric
+    The league's era. Note that default is keyed to a fantasy stat.
+
+    lgWHIP: numeric
+    The leagues walk plus hits per inning pitched.
+ attributes. Setting the attributes on instances may result in inconsistent
+    applications of the spg function across multiple instances.
+
+    spg: dict (string: numeric)
+    The standings points gained (SPG) coefficients for basic fantsay stats. Keys are 'HR', 'W', etc.
+
+    lgBA: numeric
+    The league's batting average
+
+    replacement_level: dict (string: numeric)
+    The raw SPG value for each player who is replacement level at their position.
+
+    '''
+
+    spg = {
+        'K': 17.5,
+        'xER': 8.5,
+        'xWHIP': 12.4,
+        'W': 1.7,
+        'S': 4.5,
+        'SB': 3.5,
+        'HR': 5.3,
+        'xH': 8.5,
+        'RBI': 13.4,
+        'R': 11.7
+        }
+    lgERA = 3.79
+    lgWHIP = 1.23
+    replacement_level = {
+        'P': 11.4,
+        'C': 9,
+        '1B': 16.7,
+        '2B': 16.7,
+        '3B': 16.7,
+        'SS': 16.7,
+        'OF': 16.7,
+        'RF': 16.7,
+        'CF': 16.7,
+        'LF': 16.7,
+        'U': 16.7,
+        }
+
+    spgxH = 8.475##used 8.47, nearly matched the real value of 8.486 in 2019.
+    spgHR = 5.286##real value was 4.782
+    spgR = 11.671###used 10.81,real value was 12.532
+    spgRBI = 13.38##used 12.20,was 14.564
+    spgSB = 3.46##used 4.27,was 2.657
+    lgBA = .265
+
+    _standard_pitching_keys = {i : i for i in ['K','W','S','ER','IP','BB','H','ERA','WHIP']}
+    _standard_hitting_keys = {i : i for i in ['H','HR','R','RBI','SB','BA','AB']}
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def fromRate(rate, denominator, to_int=False):
+        '''
+        Returns a counting-type stat from a rate and a denominator.
+        Note: this is essentially the product of the two arguments.
+        '''
+        out = rate * denominator
+        if to_int:
+            out = int(round(out))
+        return out
+
+    @staticmethod
+    def getRate(numerator, denominator, to_int=False, allow_inf=True):
+        '''
+        Gets a rate stat, e.g. K/IP, from arguments.
+        
+        '''
+        out = np.divide(numerator,denominator)
+        if not allow_inf:
+            if out == np.inf:
+                raise ZeroDivisionError("Denominator in getRate was zero. Try 'allow_inf' = True")
+        if to_int:
+            try:
+                out = int(round(out))
+            except OverflowError:
+                pass
+        return out
+
+    def Kper9(self, pitcher_stats, stat_dict=None, allow_inf = True):
+        '''
+        Returns strikeouts per nine innnings from a dict-like of pitcher stats.
+
+        Parameters
+        ----------
+        pitcher_stats: dict_like 
+        
+        Pitcher stats that includes strikeouts and innings pitched.
+
+        stat_dict: dictionary, default = None
+
+        A dictionary mapping 'K' and/or 'IP' to names for those stats in pitcher_stats or None. Used when pitcher_stats uses names
+        besides 'K' and 'IP' for strikeouts and innings pitched. If None, uses 'K' and 'IP' as pitcher_stats keys.
+
+
+        '''
+        
+        if not stat_dict:
+            return getRate(pitcher_stats['K'], pitcher_stats['IP']/9, allow_inf=allow_inf)
+        else:
+            stat_dict = {**_standard_pitching_keys, **stat_dict}
+            K = pitcher_stats(stat_dict['K'])
+            IP = pitcher_stats(stat_dict['IP'])
+            return getRate(K,IP/9, allow_inf = allow_inf)
+
+    def BBper9(self, pitcher_stats, stat_dict=None, allow_inf = True):
+        '''
+        Returns walks per nine innnings from a dict-like of pitcher stats.
+
+        Parameters
+        ----------
+        pitcher_stats: dict_like 
+        
+        Pitcher stats that includes walks and innings pitched.
+
+        stat_dict: dictionary, default = None
+
+        A dictionary mapping 'BB' and/or 'IP' to the names used for those in pitcher_stats, or None. Use if pitcher_stats uses names
+        besides 'BB' and 'IP' for walks and innings pitched. If None, uses 'BB' and 'IP' as pitcher_stats keys.
+        '''
+        if not stat_dict:
+            return getRate(pitcher_stats['BB'], pitcher_stats['IP']/9, allow_inf = allow_inf)
+        else:
+            stat_dict = {**_standard_pitching_keys, **stat_dict}
+            BB = pitcher_stats[stat_dict['BB']]
+            IP = pitcher_stats[stat_dict['IP']]
+            return getRate(BB,IP/9, allow_inf = allow_inf)
+
+    def FIP(self, pitcher_stats, fip_const = 3.20, stat_dict = None):
+        if not stat_dict:
+            K = pitcher_stats['K']
+            BB = pitcher_stats['BB']
+            IP = pitcher_stats['IP']
+            HR = pitcher_stats['HR']
+        else:
+            stat_dict = {**_standard_pitching_keys, **stat_dict}
+            K = pitcher_stats[stat_dict['K']]
+            BB = pitcher_stats[stat_dict['BB']]
+            IP = pitcher_stats[stat_dict['IP']]
+            HR = pitcher_stats[stat_dict['HR']]
+        FIP = np.divide((3*BB - 2*K + 13 * HR),IP) + fip_const
+        return FIP
+
+    def pitcherFWAR(self, pitcher_stats, position = 'P', use_count_stats = True, stat_dict = None):
+        '''
+        Returns a cummulative SPG value, adjusted for replacement level, from a pitcher's stats.
+        
+        Parameters
+        ----------
+        pitcher_stats: dict_like
+            A list of pitcher stats that includes strikeouts, walks, hits, earned runs, innings pitched, wins, and saves. 
+        
+        position: string, default = 'P'
+            A string for the player's positon. Used to calculate replacement level value.
+        
+        use_count_stats: bool, default = True
+            Use counting stats (ER, W, H) for calculations if True; use rate stats (ERA, WHIP) if False.
+
+        stat_dict: dictionary, default = None
+            A dictionary map of standard keys to the keys used for those stats in player_stats. 
+            The standard keys are 'W', 'S', 'K', 'BB', 'IP', 'H', 'ER', 'ERA', 'WHIP'. This can be
+            a partial map; standard keys are used when stat_dict contains no entry for that stat.
+        
+
+        '''
+        if use_count_stats:
+            if not stat_dict:
+                K = pitcher_stats['K']
+                W = pitcher_stats['W']
+                S = pitcher_stats['S']
+                ER = pitcher_stats['ER']
+                BB = pitcher_stats['BB']
+                H = pitcher_stats['H']
+                IP = pitcher_stats['IP']
+                RA = H + BB #RA = runners allowed.
+            else:
+                stat_dict = {**self._standard_pitching_keys, **stat_dict}
+                K = pitcher_stats[stat_dict['K']]
+                W = pitcher_stats[stat_dict['W']]
+                S = pitcher_stats[stat_dict['S']]
+                ER = pitcher_stats[stat_dict['ER']]
+                BB = pitcher_stats[stat_dict['BB']]
+                H = pitcher_stats[stat_dict['H']]
+                IP = pitcher_stats[stat_dict['IP']]
+                RA = H + BB
+        else:
+            if not stat_dict:
+                K = pitcher_stats['K']
+                W = pitcher_stats['W']
+                S = pitcher_stats['S']
+                ER = pitcher_stats['ERA']*pitcher_stats['IP']/9
+                RA = pitcher_stats['WHIP']*pitcher_stats['IP']
+                IP = pitcher_stats['IP']
+            else:
+                stat_dict = {**self._standard_pitching_keys, **stat_dict}
+                K = pitcher_stats[stat_dict['K']]
+                W = pitcher_stats[stat_dict['W']]
+                S = pitcher_stats[stat_dict['S']]
+                IP = pitcher_stats['IP']
+                ER = pitcher_stats[stat_dict['ERA']]*IP/9
+                RA = pitcher_stats['WHIP']*IP
+                
+        K_ = K/self.spg['K']
+        W_ = W/self.spg['W']
+        S_ = S/self.spg['S']
+        xER_ = ((self.lgERA * IP) - ER)/self.spg['xER']
+        xWHIP_ = ((self.lgWHIP * IP) - (RA))/self.spg['xWHIP']
+        return K_ + W_ + S_ + xER_ + xWHIP_ - self.replacement_level[position]
+
+    def hitterFWAR(self, hitter_stats, position = 'U', use_count_stats = True, stat_dict = None):
+        '''
+        Returns a cummulative SPG value, adjusted for replacement level, from a hitter's stats.
+        ##FIX ME
+        Current documentation incomplete. See pitcherFWAR for some guidance.
+        '''
+        if use_count_stats:
+            if not stat_dict:
+                HR = hitter_stats['HR']
+                SB = hitter_stats['SB']
+                RBI = hitter_stats['RBI']
+                R = hitter_stats['R']
+                H = hitter_stats['H']
+                AB = hitter_stats['AB']
+            else:
+                stat_dict = {**self._standard_hitting_keys, **stat_dict}
+                HR = hitter_stats[stat_dict['HR']]
+                SB = hitter_stats[stat_dict['SB']]
+                RBI = hitter_stats[stat_dict['RBI']]
+                R = hitter_stats[stat_dict['R']]
+                H = hitter_stats[stat_dict['H']]
+                AB = hitter_stats[stat_dict['AB']]
+
+        else:
+            if not stat_dict:
+                HR = hitter_stats['HR']
+                SB = hitter_stats['SB']
+                RBI = hitter_stats['RBI']
+                R = hitter_stats['R']
+                AB = hitter_stats['AB']
+                H = hitter_stats['BA']*AB
+            else:
+                stat_dict = {**self._standard_hitting_keys, **stat_dict}
+                HR = hitter_stats[stat_dict['HR']]
+                SB = hitter_stats[stat_dict['SB']]
+                RBI = hitter_stats[stat_dict['RBI']]
+                R = hitter_stats[stat_dict['R']]
+                AB = hitter_stats[stat_dict['AB']]
+                H = hitter_stats[stat_dict['BA']]*AB
+                
+        HR_ = HR/self.spg['HR']
+        RBI_ = RBI/self.spg['RBI']
+        R_ = R/self.spg['RBI']
+        SB_ = SB/self.spg['SB']
+        xH_ = (H - self.lgBA*AB)/self.spg['xH']
+        return HR_ + RBI_ + R_ + SB_ + xH_ - self.replacement_level[position]
+
+
+
+
+
+
--- testingScript.py
+++ testingScript.py
@@ -5,124 +5,22 @@ Created on Tue Feb 26 06:15:46 2019
 @author: lenha
 """
 
-from BaseClasses.BaseFantasyClasses import ClosersFile
-from BaseClasses.BaseFantasyClasses import DepthPitcherFile
-from BaseClasses.Pitcher import Pitcher
-from BaseClasses.Team import Team
-from DraftAssistant import DraftAssistant
-import os
-import datetime
-import re
+import pandas as pd
 
-def test1():
-    '''Testing closers file'''
-    closers = ClosersFile("closers2019.csv")
-    closers.test()
-    pitchers = DepthPitcherFile("TestFiles/pitchersProj.csv")
-    Pitcher.replacementLevel = pitchers.replacementLevel()
-    closers.guysAsPitchers(pitchers)
-    #print(closers.listNonPitchers())
-    for team in closers.teams:
-        try:
-            guy = team.closer.name
-            fWAR = team.closer.fWAR()
-        except:
-            guy = "none"
-            fWAR = 0.0
-        print("{:12.12} {:15} {:4.2f}".format(team.team,guy,fWAR))
-        for guy in team.handcuffs:
-            try:
-                #print(guy.isaPitcher())
-                dude = guy.name
-                fWAR = guy.fWAR()
-                print("{:12.12} {:15.15} {:>5.2f} good".format(team.team,dude,fWAR))
-                
-            except:
-                #print(guy)
-                #guy = guy
-                fWAR = 0.0
-                print("{:12.12} {:15.15} {:>5.1f} bad".format(team.team,guy,fWAR))
+from CBS_fantasy_baseball.stats import StatCalculator
 
-    for guy in closers.closersWithStrength("strong") + closers.closersWithStrength("medium") + closers.closersWithStrength("weak"):
-        print("{:15.15}  {:2} {:5.1f}".format(guy.name, guy.SV, guy.fWAR()))
-    
-    print("** Battlers **")
-    for team in closers.teams:
-        if team.strength.lower() == "battle":
-            for guy in team.handcuffs:
-                try:
-                    print("{:8.8}  {:15.15}  {:2} {:5.1f}".format(team.team, guy.name, guy.SV, guy.fWARc()))
-                except:
-                    pass
-    print("** End of test one.")
-    
-    
-def test2():
-    '''Testing Team functionality'''
+def test_StatCalculator():
+    calc = StatCalculator()
+    jackie = {'HR': 12, 'RBI': 48,'R': 120, 'SB': 29, 'AB': 590, 'H': 175, 'BA': 175/590, 'hr': 12}
+    stat_dict = {'HR': 'HR', 'RBI': 'RBI','R': 'R', 'SB': 'SB', 'AB': 'AB', 'H': 'H', 'BA': 'BA', 'hr': 'HR'}
+    calc.hitterFWAR(jackie)
+    calc.hitterFWAR(jackie, use_count_stats = False, stat_dict=stat_dict)
+    satchel = {'K': 43,'IP': 72.2, 'BB': 22, 'H': 61, 'ER': 20, 'ERA': 2.48, 'SV': 1, 'S': 1, 'W': 6}
+    calc.pitcherFWAR(satchel)
+    calc.pitcherFWAR(satchel, use_count_stats=False)
+    satchel.pop('K')
+    satchel['SO'] = 43
+    calc.pitcherFWAR(satchel, stat_dict = {'K':'SO'})
 
-def reTest():
-    a = "University"
-    reg = "univ"
-    b = re.match(reg,a,re.I)
-    print(b)
-    
-def assistantTest():
-    assist = DraftAssistant(#"Files/stats-main2017hitters.csv",
-                   #"TestFiles/hittersOwned.csv",
-                   #"TestFiles/pitchersOwned.csv",
-                   #"horseShitHitters.csv",
-                   #"horseShitPitchers.csv",
-                   "dummyHitters.csv",
-                   "dummyPitchers.csv",
-                   "Projections/hittersProj_2_27_2019.csv",
-                   "Projections/pitcherProj_2_27_2019.csv"
-                   )
-    
-    dlfldr = "/Users/Lenhart/Downloads/2019"
-    
-    #print(assist.eligHitters.repeatedPlayerNames)
-    #print(assist.eligPitchers.repeatedPlayerNames)
-    assist.teamName = "Omak Wrong Players"
-    assist.setMyTeam()
-    assist.myTeam.setLineUp()
-    assist.myHandcuffs()
-    assist.basicAnalysis()
-    print(assist.fileIsPitchers('dummyPitchers.csv'))
-    print(assist.fileIsHitters('dummyHitters.csv'))
-    assist.update('dummyHitters.csv','dummyPitchers.csv')
-    assist.update('','')
-    assist.klfTeams()
 
-def fileosTest():
-    files = []
-    print(os.path.abspath('.'))
-    path = "/Users/lenhart/Downloads/2019"
-    #path = "."
-    with os.scandir(path) as it:
-        for e in os.scandir(path):
-            files.append(e)
-    it.close()
-    files.sort(key = lambda file: file.stat().st_ctime)
-    #for e in files:
-    #    print(e, e.stat().st_ctime)
-    dudes = open(files[-1],'r')
-    dudes.close()
-    print(datetime.datetime.utcfromtimestamp(files[-1].stat().st_ctime))
-    print(os.name)
-    #print(dudes.readline())
-    #print(dudes.readline())
-    #x = open(files[-2],'r')
-    #print(x.readline())
-    #print(type(e))
-    #print(isinstance(e,os.DirEntry))
-    
-def irrelevant():
-    print("{}{}".format("1\n1","1\n1"))
-        
-    
-
-    
-#test1()
-#assistantTest()
-#fileosTest()
-reTest()
+test_StatCalculator()
