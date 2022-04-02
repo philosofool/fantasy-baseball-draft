38470540453125d148e91ce8f63bca1a5f84c809
stephen j. lenhart <lenhart.lenhart@gmail.com>
2022-04-02 15:47:31 -0700

    Move site URL to config

--- fantasy_baseball_draft/extract.py
+++ fantasy_baseball_draft/extract.py
@@ -24,7 +24,7 @@ def nav_to_cbs(browser: Firefox) -> Firefox:
     """Login to CBS and load main page.
     
     This authenticates a headless browser for successive calls."""
-    browser.get("https://www.cbssports.com/login?product_abbrev=mgmt&xurl=https%3A%2F%2Fklf2006.baseball.cbssports.com%2F&master_product=38994")
+    browser.get(config['cbs_home'])
     user = browser.find_element_by_id('userid')
     password = browser.find_element_by_id('password')
     button = browser.find_element_by_name('_submit')
@@ -35,7 +35,7 @@ def nav_to_cbs(browser: Firefox) -> Firefox:
 
 def download_cbs_hitters(browser) -> Firefox:
     """Download cbs hitters."""    
-    batter_true_talent = "https://klf2006.baseball.cbssports.com/stats/stats-main/all:C:1B:2B:3B:SS:MI:CI:LF:CF:RF:U/ytd:p/True+Talent?"
+    batter_true_talent = config["cbs_hitters"] 
     browser.get(batter_true_talent)
     browser.find_element(By.ID, 'btnExport').click()
     save_as = f'cbs_hitters_{pd.Timestamp.now().strftime("%Y-%m-%d")}'
