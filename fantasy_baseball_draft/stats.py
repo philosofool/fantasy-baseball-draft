cc54ef95e2046627938fde875b1a0c512cc22f6c
philosofool <66087178+philosofool@users.noreply.github.com>
2023-03-04 07:29:21 -0700

    Switch stats-fwar to spg products
    - They were quitients. However, linear regressions are slopes and
    coefficients work as products. This is just simpler.

--- fantasy_baseball_draft/stats.py
+++ fantasy_baseball_draft/stats.py
@@ -2,21 +2,21 @@
 
 def pitcher_fwar(pitcher, spg, league):
     fwar = (
-        pitcher.k / spg['so'] 
-        + pitcher.w / spg['w']
-        + pitcher.s / spg['s']
-        + ((league['era'] - pitcher.era)/9 * pitcher.inn / spg['xer']).fillna(0)
-        + ((league['whip'] - pitcher.whip) * pitcher.inn / spg['xwhip']).fillna(0)
+        pitcher.k * spg['so'] 
+        + pitcher.w * spg['w']
+        + pitcher.s * spg['s']
+        + ((league['era'] - pitcher.era)/9 * pitcher.inn * spg['er']).fillna(0)
+        + ((league['whip'] - pitcher.whip) * pitcher.inn * spg['whip']).fillna(0)
     )
     return fwar
 
 def hitter_fwar(hitter, spg, league):
-    ba = hitter.h /hitter.ab
+    ba = hitter.h / hitter.ab
     fwar = (
-        hitter.r / spg['r']
-        + hitter.hr / spg['hr']
-        + hitter.rbi / spg['rbi']
-        + hitter.sb / spg['sb']
-        + ((ba - league['ba']) * hitter.ab / spg['xh']).fillna(0)
+        hitter.r * spg['r']
+        + hitter.hr * spg['hr']
+        + hitter.rbi * spg['rbi']
+        + hitter.sb * spg['sb']
+        + ((ba - league['ba']) * hitter.ab * spg['ba']).fillna(0)
     )
     return fwar
