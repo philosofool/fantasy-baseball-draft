c2c98070ab2cd996511401b1bad32db80a6dce3e
philosofool <66087178+philosofool@users.noreply.github.com>
2023-03-07 17:17:25 -0700

    standardize stat names in )fwar funcs

--- fantasy_baseball_draft/stats.py
+++ fantasy_baseball_draft/stats.py
@@ -7,12 +7,14 @@ def pitcher_fwar(pitcher, spg):
         pitcher.k * spg['k'] 
         + pitcher.w * spg['w']
         + pitcher.s * spg['s']
-        + ((league['era'] - pitcher.era)/9 * pitcher.inn * spg['er']).fillna(0)
-        + ((league['whip'] - pitcher.whip) * pitcher.inn * spg['whip']).fillna(0)
+        + ((league['era'] - pitcher.era)/9 * pitcher.ip * spg['era']).fillna(0)
+        + ((league['whip'] - pitcher.whip) * pitcher.ip * spg['whip']).fillna(0)
     )
     return fwar
 
-def hitter_fwar(hitter, spg, league):
+def hitter_fwar(hitter, spg):
+    league = spg['median']
+    spg = spg.spg
     ba = hitter.h / hitter.ab
     fwar = (
         hitter.r * spg['r']
