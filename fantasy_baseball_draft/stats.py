7cf5e485afeec41214bab4ee16c32671a8df866a
Stephen Lenhart <stephen.lenhart@lockerdome.com>
2024-02-24 11:13:52 -0700

    Move StatSynonyms to Stats

--- fantasy_baseball_draft/stats.py
+++ fantasy_baseball_draft/stats.py
@@ -1,13 +1,47 @@
 """Handle stats."""
 
 import pandas as pd
+import numpy as np
+from numpy.typing import ArrayLike
+
+
+class StatSynonyms:
+    """Concordance of stat synonyms used in baseball.
+
+    Example usage:
+        stat_synonyms = StatSynonyms()
+        assert stat_synonyms.normalize('SO') == 'K'
+    """
+    def __init__(self):
+        self.syn_set = {
+            'AVG': 'BA',
+            'INN': 'IP',
+            'INNS': 'IP',
+            'BBI': 'BB',
+            'SO': 'K',
+            'SV': 'S',
+            'APP': 'G',
+            'PLAYERID': 'playerid'
+        }
+
+    def normalize(self, abbr: str) -> str:
+        return self.syn_set.get(self.preprocess(abbr), abbr)
+
+    def preprocess(self, value):
+        return value.replace('.', '').upper()
+
+    def normalize_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Normalize dataframe column names."""
+        df = df.copy()
+        return df.rename(columns={k: self.normalize(k) for k in df.columns})
+
 
 def pitcher_fwar(pitcher: pd.DataFrame, spg: pd.DataFrame) -> pd.Series:
     """Return pitcher spg weighted values."""
     league = spg['median']
     spg = spg['spg']
     fwar = (
-        pitcher.k * spg['k'] 
+        pitcher.k * spg['k']
         + pitcher.w * spg['w']
         + pitcher.s * spg['s']
         + ((pitcher.era - league['era'])/9 * pitcher.ip * spg['era']).fillna(0)
--- fantasy_baseball_draft/utils.py
+++ fantasy_baseball_draft/utils.py
@@ -6,7 +6,8 @@ Utilties for fantasy baseball.
 import pandas as pd
 import os
 
-from philosofool.data_science.clean import Concordance
+from stats import StatSynonyms
+
 
 class DataLoader:
 
@@ -33,6 +34,7 @@ def load_cbs_data(path) -> pd.DataFrame:
     df.Player = df.Player.str.strip()
     return df
 
+
 def cbs_player_col_to_df(player: pd.Series) -> pd.DataFrame:
     """Transform Player column to a DataFrame."""
     name_re = "(.+) ([123]B|\w{1,2}) \|\s(\w{2,3})"
@@ -42,37 +44,3 @@ def cbs_player_col_to_df(player: pd.Series) -> pd.DataFrame:
         .rename(columns={i: group for i, group in enumerate(groups)})
         .set_index(player.index)
     )
-
-class StatSynonyms(Concordance):
-    """Concordance of stat synonyms used in baseball.
-
-    Example usage:
-        stat_synonyms = StatSynonyms()
-        assert stat_synonyms.normalize('SO') == 'K'
-    """
-    def __init__(self):
-        ...
-        self.syn_set = {
-            'AVG': 'BA',
-            'INN': 'IP',
-            'INNS': 'IP',
-            'BBI': 'BB',
-            'SO': 'K',
-            'SV': 'S',
-            'APP': 'G',
-            'PLAYERID': 'playerid'
-        }
-
-    def normalize(self, abbr):
-        return self.syn_set.get(self.preprocess(abbr), abbr)
-
-    def preprocess(self, value):
-        return value.replace('.', '').upper()
-
-    def normalize_df(self, df, inplace=False):
-        """Normalize dataframe column names."""
-        if not inplace:
-            df = df.copy()
-            self.normalize_df(df, True)
-            return df
-        df.rename(columns={k: self.normalize(k) for k in df.columns}, inplace=True)
